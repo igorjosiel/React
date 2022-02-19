acf9cece6901d428d3f9745e243a34254dbccbc5
Igor Josiel <igorjosielvitalino@gmail.com>
2022-02-19 14:50:46 -0300

    feat: calling to the github's route to list all my repositories that I have there, and showing some informations of them. Then, I transformed my files for typescript, so that these ones can be safer.

--- 1-github-explorer/src/components/RepositoryList.tsx
+++ 1-github-explorer/src/components/RepositoryList.tsx
@@ -3,16 +3,20 @@ import RepositoryItem from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/igorjosiel/repos')
             .then(response => response.json())
             .then(data => setRepositories(data));
     }, []);
-
-    console.log('data: ', repositories);
 
     return (
         <section className="repository-list">
