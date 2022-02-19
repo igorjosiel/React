5fbea4622751f0977ff3e2398e937c7716afd83c
Igor Josiel <igorjosielvitalino@gmail.com>
2022-02-18 22:09:38 -0300

    feat: fetching for all my github's repositories and showing it on the page using hooks to do this.

--- 1-github-explorer/src/components/RepositoryItem.jsx
+++ 1-github-explorer/src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 const RepositoryItem = (props) => {
     return (
         <li>
-            <strong>{props.repository.name ?? 'Default'}</strong>
+            <strong>{props.repository.name}</strong>
             <strong>{props.repository.description}</strong>
 
-            <a href={props.link}>Acessar repositório</a>
+            <a href={props.repository.html_url}>Acessar repositório</a>
         </li>
     )
 }
