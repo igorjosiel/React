fef59c414013d37886ca85a7ff228ea9a7ec7d24
Igor Josiel <igorjosielvitalino@gmail.com>
2022-03-13 22:59:37 -0300

    Learn about the different ways to consume the route of an API from the front end with the next framework, and which is the best in each case.

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  const { priceId, amount } = product;
+
   return (
     <>
       <Head>
@@ -14,14 +25,35 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={priceId} />
         </section>
 
         <img src='/images/avatar.svg' alt='Girl coding' />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KckJIKURehvr7P6iJyEQD9Y', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
