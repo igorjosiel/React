6a8ccebc5df84ffbc7a0aafe695577099cf96d3c
Igor Josiel <igorjosielvitalino@gmail.com>
2022-03-24 17:58:22 -0300

    fix: Configuring the GitHub keys to authenticate with my Github account.

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,11 +1,14 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data } = useSession();
 
-    return isUserLoggedIn ? (
+    console.log('Foi: ', data);
+
+    return data.session ? (
         <button
             type="button"
             className={styles.signInButton}
@@ -15,7 +18,7 @@ export function SignInButton() {
             <FiX color="#737388" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.SignInButton}>
+        <button type="button" className={styles.SignInButton} onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
